5e0f43a4d1e311badfe87b15e09c2586883376aa
shotaro <134831718+ShotaroKondo@users.noreply.github.com>
2023-09-03 02:27:53 +0000

    Add custom hook

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,48 +1,29 @@
-// import { FC } from "react"
-// import { ChangeEvent, FC, useState } from "react";
 import { styled } from "styled-components";
 import { ChangeEvent, FC, useState, useCallback } from "react";
 import { MemoList } from "./MemoList";
+import { useMemoList } from "../hooks/useMemoList";
 
 export const App: FC = () => {
-  const [memos, setMemos] = useState<string[]>([]);
+  const { memos, addTodo, deleteTodo } = useMemoList();
   const [text, setText] = useState<string>("");
   const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
     setText(e.target.value);
   const onClickAdd = () => {
-    const newMemos = [...memos];
-    newMemos.push(text);
-    setMemos(newMemos);
+    addTodo(text);
     setText("");
   };
-  // const onClickDelete = (index: number) => {
+  
   const onClickDelete = useCallback(
     (index: number) => {
-    const newMemos = [...memos];
-    newMemos.splice(index, 1);
-    setMemos(newMemos);
-  // };
-      },
-    [memos]
+        deleteTodo(index);
+    },
+    [deleteTodo]
   );
   return (
 		<div>
 			<h1>簡単メモアプリ</h1>
       <input type="text" value={text} onChange={onChangeText} />
       <SButton onClick={onClickAdd}>追加</SButton>
-      {/* <SContainer>
-        <p>メモ一覧</p>
-        <ul>
-          {memos.map((memo, index) => (
-            <li key={memo}>
-              <SMemoWrapper>
-                <p>{memo}</p>
-                <SButton onClick={() => onClickDelete(index)}>削除</SButton>
-              </SMemoWrapper>
-            </li>
-          ))}
-        </ul>
-      </SContainer> */}
       <MemoList memos={memos} onClickDelete={onClickDelete} />
 		</div>
 	);
@@ -51,13 +32,3 @@ export const App: FC = () => {
 const SButton = styled.button`
   margin-left: 16px;
 `;
-
-// const SContainer = styled.div`
-//   border: solid 1px #ccc;
-//   padding: 16px;
-//   margin: 8px;
-// `;
-// const SMemoWrapper = styled.div`
-//   display: flex;
-//   align-items: center;
-// `;
