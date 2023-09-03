76ae1b72ca1333d8a54ed6015bf9688f57f6fa4e
shotaro <134831718+ShotaroKondo@users.noreply.github.com>
2023-09-03 02:25:02 +0000

    Add memo list components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,8 @@
 // import { FC } from "react"
-import { ChangeEvent, FC, useState } from "react";
+// import { ChangeEvent, FC, useState } from "react";
 import { styled } from "styled-components";
+import { ChangeEvent, FC, useState, useCallback } from "react";
+import { MemoList } from "./MemoList";
 
 export const App: FC = () => {
   const [memos, setMemos] = useState<string[]>([]);
@@ -13,17 +15,22 @@ export const App: FC = () => {
     setMemos(newMemos);
     setText("");
   };
-  const onClickDelete = (index: number) => {
+  // const onClickDelete = (index: number) => {
+  const onClickDelete = useCallback(
+    (index: number) => {
     const newMemos = [...memos];
     newMemos.splice(index, 1);
     setMemos(newMemos);
-  };
+  // };
+      },
+    [memos]
+  );
   return (
 		<div>
 			<h1>簡単メモアプリ</h1>
       <input type="text" value={text} onChange={onChangeText} />
       <SButton onClick={onClickAdd}>追加</SButton>
-      <SContainer>
+      {/* <SContainer>
         <p>メモ一覧</p>
         <ul>
           {memos.map((memo, index) => (
@@ -35,7 +42,8 @@ export const App: FC = () => {
             </li>
           ))}
         </ul>
-      </SContainer>
+      </SContainer> */}
+      <MemoList memos={memos} onClickDelete={onClickDelete} />
 		</div>
 	);
 };
@@ -44,12 +52,12 @@ const SButton = styled.button`
   margin-left: 16px;
 `;
 
-const SContainer = styled.div`
-  border: solid 1px #ccc;
-  padding: 16px;
-  margin: 8px;
-`;
-const SMemoWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`;
+// const SContainer = styled.div`
+//   border: solid 1px #ccc;
+//   padding: 16px;
+//   margin: 8px;
+// `;
+// const SMemoWrapper = styled.div`
+//   display: flex;
+//   align-items: center;
+// `;
