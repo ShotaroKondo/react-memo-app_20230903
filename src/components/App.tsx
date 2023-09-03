6cbafcbe9cf1f5e666ddf455934e10b26d39e036
shotaro <134831718+ShotaroKondo@users.noreply.github.com>
2023-09-03 02:19:54 +0000

    Add memo list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,12 @@ import { ChangeEvent, FC, useState } from "react";
 import { styled } from "styled-components";
 
 export const App: FC = () => {
-   const [text, setText] = useState<string>("");
+  const [memos, setMemos] = useState<string[]>([
+    "テスト1",
+    "テスト2",
+    "テスト3",
+  ]);
+  const [text, setText] = useState<string>("");
   const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
     setText(e.target.value);
   const onClickAdd = () => {
@@ -13,12 +18,35 @@ export const App: FC = () => {
   return (
 		<div>
 			<h1>簡単メモアプリ</h1>
-            <input type="text" value={text} onChange={onChangeText} />
+      <input type="text" value={text} onChange={onChangeText} />
       <SButton onClick={onClickAdd}>追加</SButton>
+      <SContainer>
+        <p>メモ一覧</p>
+        <ul>
+          {memos.map((memo, index) => (
+            <li key={memo}>
+              <SMemoWrapper>
+                <p>{memo}</p>
+                <SButton>削除</SButton>
+              </SMemoWrapper>
+            </li>
+          ))}
+        </ul>
+      </SContainer>
 		</div>
 	);
 };
 
 const SButton = styled.button`
   margin-left: 16px;
+`;
+
+const SContainer = styled.div`
+  border: solid 1px #ccc;
+  padding: 16px;
+  margin: 8px;
+`;
+const SMemoWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `;
