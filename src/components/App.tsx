12f50055f9c471eaacb9286329e4af3f146af7c7
shotaro <134831718+ShotaroKondo@users.noreply.github.com>
2023-09-03 02:18:40 +0000

    Add text box

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,24 @@
-import { FC } from "react"
+// import { FC } from "react"
+import { ChangeEvent, FC, useState } from "react";
+import { styled } from "styled-components";
 
 export const App: FC = () => {
+   const [text, setText] = useState<string>("");
+  const onChangeText = (e: ChangeEvent<HTMLInputElement>) =>
+    setText(e.target.value);
+  const onClickAdd = () => {
+    alert(text + " added!");
+    setText("");
+  };
   return (
 		<div>
 			<h1>簡単メモアプリ</h1>
+            <input type="text" value={text} onChange={onChangeText} />
+      <SButton onClick={onClickAdd}>追加</SButton>
 		</div>
 	);
 };
+
+const SButton = styled.button`
+  margin-left: 16px;
+`;
